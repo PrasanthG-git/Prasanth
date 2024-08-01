330073121b90ec9e81c7d52c929f80a6f54d96e2
PrasanthG-git <prasanthg15902@gmail.com>
2024-08-01 10:44:39 +0530

    git added:1.js

---
+++ first day/script.js
@@ -0,0 +1,9 @@
+console.log("js and html work fine");
+alert("the emergency");
+// just to show some information => done not give any output
+let m = confirm("are you sure");
+// we can use this to get a confirmation => will give output in boolean value.
+console.log(m);
+const name = prompt("enter your name");
+console.log(name);
+//two button shown in the dialog box "ok" and "cancel" along with the textbox => the user input is aways in string.
---
+++ second day/script.js
@@ -0,0 +1,50 @@
+// let a = prompt("Enter the number");
+// if (a % 2 == 0)
+// {
+//     console.log("the Entered number is even");
+// }
+// else
+// {
+//     console.log("the Entered number is odd");
+// }
+// const arr = [1, 3, 5, 7, 9];
+// let g = arr.length;
+// for (let i = 0; i < g; i++)
+// {
+//     switch (arr[i]) {
+//       case 1:
+//         console.log("odd");
+//         break;
+//       case 2:
+//         console.log("Even");
+//         break;
+//       case 3:
+//         console.log("odd");
+//         break;
+//       case 4:
+//         console.log("Even");
+//         break;
+//       case 5:
+//         console.log("odd");
+//         break;
+//       case 6:
+//         console.log("Even");
+//         break;
+//       case 7:
+//         console.log("odd");
+//         break;
+//       case 8:
+//         console.log("Even");
+//         break;
+//       case 9:
+//         console.log("odd");
+//             break;
+//         default:
+//             console.log("over come");
+//             break;
+
+//     }
+// }
+add(n1, n2);
+{
+}
